67ad2eef38c9393dc74b86c2e8a90188802d07f9
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-04-10 22:24:51 +0545

    .

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -44,7 +44,7 @@ const Sidebar = () => {
 
               <li>
                 <a
-                  href="#"
+                  href=""
                   className="group relative flex justify-center rounded-sm px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
                 >
                   <svg
