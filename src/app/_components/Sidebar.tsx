db9330de66639e66bf93936bb10413ccf8bfddb7
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-04-11 23:54:52 +0545

    .

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
 
         <div className="border-t border-gray-100">
           <div className="px-2">
-            <ul className="space-y-1 border-t border-gray-100 pt-4">
+            <ul className="space-y-1 border-t border-gray-100 ">
               <li>
                 <a
                   href=""
