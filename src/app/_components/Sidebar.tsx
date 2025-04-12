f9fb4be6a514ea54a08b4b99189a452668e4bb18
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-04-12 21:00:34 +0545

    .

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -77,7 +77,7 @@ const Sidebar = () => {
                     xmlns="http://www.w3.org/2000/svg"
                     className="size-5 opacity-75"
                     fill="none"
-                    viewBox="0 0 24 24"
+                    viewBox="0 0 24"
                     stroke="currentColor"
                     strokeWidth="2"
                   >
