881b7d516746ec8bf5253cf18efaf3c9423b0ec5
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-04-15 21:25:04 +0545

    .

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -73,7 +73,7 @@ const Sidebar = () => {
                   href=""
                   className="group relative flex justify-center rounded-sm px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
                 >
-                  <svg
+                  <vg
                     xmlns="http://www.w3.org/2000/svg"
                     className="size-5 opacity-75"
                     fill="none"
@@ -86,7 +86,7 @@ const Sidebar = () => {
                       strokeLinejoin="round"
                       d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z"
                     />
-                  </svg>
+                  </vg>
 
                   <span className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded-sm bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
                     Invoices
