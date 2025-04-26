60c551d2a20d619f86e352e4efdd5323ac7dff5f
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-04-26 22:03:38 +0545

    .

--- src/app/(dashboard)/(routes)/layout.tsx
+++ src/app/(dashboard)/(routes)/layout.tsx
@@ -3,10 +3,7 @@ import Sidebar from "@/app/_components/Sidebar";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid grid-cols-[7.5rem_1fr] grid-rows
-    
-    
-    -[auto_1fr] ">
+    <div className="grid grid-cols-[7.5rem_1fr] grid-rows-[auto_1fr] ">
       <Header />
       <Sidebar />
       <div className="flex flex-col items-center">{children}</div>
