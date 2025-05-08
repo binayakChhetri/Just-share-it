6578e47e7eec98efce613322955cbef9665cb5c6
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-05-08 14:14:11 +0545

    .

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -6,12 +6,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req: Request) {
   const { emailToSend, fileName, fileSize, fileType, url, sendTime } =
     await req.json();
-
-  if (emailToSend) console.log(emailToSend);
   try {
     const { data, error } = await resend.emails.send({
-      from: "justshareit@resend.dev",
-      to: ["binayakchhetri1234@gmail.com"],
+      from: "noreply@justshare.dev",
+      to: emailToSend,
       subject: "File shared with you",
       react: EmailTemplate({
         fileName,
@@ -23,6 +21,9 @@ export async function POST(req: Request) {
       }),
     });
 
+    if (data) {
+      console.log(data);
+    }
     if (error) {
       return Response.json({ error }, { status: 500 });
     }
