cfa5cdd27dc0080da1e6d9ae2e0b9b6438e296c2
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-07-12 23:34:42 +0545

    .

--- src/app/_services/_file/file.ts
+++ src/app/_services/_file/file.ts
@@ -26,7 +26,7 @@ export const uploadFile = async (file: File, userId: string) => {
   // Upload the file to Supabase storage
   const { error: storageError } = await supabase.storage
     .from("files")
-    .upload(fileName, file);
+    // .upload(fileName, file);
 
   // Delete the file from Supabase storage if there was an error
   if (storageError) {
