72bd0b9f49659ad9c9c5d0c0f8fd262759b894c6
Binayak Chhetri <binayakchhetri1234@gmail.com>
2025-08-25 23:40:09 +0545

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase";
 
 export const supabaseUrl = "https://ayxfvdmgmcrawijvkjqt.supabase.co";
 const supabaseAnonKey =
